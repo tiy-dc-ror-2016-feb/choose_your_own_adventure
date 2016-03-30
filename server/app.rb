1e02153855c970c231e976a0fecb67abc2bd7ee7
Russell Osborne <russell@theironyard.com>
2016-03-30 18:13:02 -0400

    Add proper migrations

--- server/app.rb
+++ server/app.rb
@@ -10,11 +10,19 @@ require_relative "lib/adventure"
 set :static, true
 set :public_folder, Proc.new { File.join(root, "..", "client") }
 
+before do
+  content_type "application/json"
+end
+
 get "/backend" do
   "I am Groot!"
 end
+#
+# post "/backend/echo" do
+#   payload = JSON.parse(request.body.read)
+#   payload.to_json
+# end
 
-post "/backend/echo" do
-  payload = JSON.parse(request.body.read)
-  payload.to_json
+get "/backend/steps" do
+  Step.all.to_json
 end
--- server/lib/adventure/database.rb
+++ server/lib/adventure/database.rb
@@ -5,6 +5,6 @@ require 'yaml'
 if ENV["DATABASE_URL"]
   ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"])
 else
-  db_config = YAML.load(File.read(File.join(Dir.pwd, 'server', 'config', 'database.yml')))
+  db_config = YAML.load(File.read(File.join(Dir.pwd, 'server', 'db', 'database.yml')))
   ActiveRecord::Base.establish_connection(db_config[ENV['RACK_ENV']])
 end
