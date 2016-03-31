ecebbdaa67cedcd115c7dfcb7e80c0bb2aa3aabe
Russell Osborne <russell@theironyard.com>
2016-03-31 14:14:52 -0400

    Uncomment some demo code

--- server/app.rb
+++ server/app.rb
@@ -7,6 +7,7 @@ require "json"
 
 require_relative "lib/adventure"
 
+
 set :static, true
 set :public_folder, Proc.new { File.join(root, "..", "client") }
 
@@ -17,12 +18,8 @@ end
 get "/backend" do
   "I am Groot!"
 end
-#
-# post "/backend/echo" do
-#   payload = JSON.parse(request.body.read)
-#   payload.to_json
-# end
 
-get "/backend/steps" do
-  Step.all.to_json
+post "/backend/echo" do
+  payload = JSON.parse(request.body.read)
+  payload.to_json
 end
--- server/db/migrations/001_step_migration.rb
+++ server/db/migrations/001_step_migration.rb
@@ -1,5 +1,4 @@
 class StepMigration < ActiveRecord::Migration
-
   def up
     drop_table :steps if table_exists?(:steps)
     create_table :steps do |t|
